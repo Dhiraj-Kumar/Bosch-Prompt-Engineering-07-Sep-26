fddfe4c64e0eae73e061858c67957bc4196c06ec
Dhiraj.kumar <dhiraj.kumar@stackroute.in>
2026-09-07 12:58:36 +0530

    initial commit

---
+++ firstapp/Messages.py
@@ -0,0 +1,22 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.messages import AIMessage, HumanMessage, SystemMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
+
+conversation_history = [
+    SystemMessage(content="You are an AI assistant who gives answers with a bit of humour. Keep your tone professional while answering the questions. Add emojis to make your responses looks nice.")
+]
+
+while True:
+    user_input = input("You: ")
+    conversation_history.append(HumanMessage(content=user_input))
+    if user_input.lower() == "exit":
+        break
+    response = llm.invoke(conversation_history)
+    conversation_history.append(AIMessage(content=response.text))
+    print(f"AI: {response.text}")
+
+print(conversation_history)
